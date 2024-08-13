0c1c582c607cc7122deee15ea80b76ccdeccf4e4
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-13 20:33:50 +0530

    fixes service footer

--- src/components/ServiceFooter.jsx
+++ src/components/ServiceFooter.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const ServiceFooter = () => {
   return (
     <div className='h-[40vh] flex md:flex-row flex-col service-footer  md:justify-evenly justify-center gap-10 items-center w-full bg-[#149088] mt-12'>
-      <div className='md:text-left text-center text-3xl md:text-5xl text-white md:flex-col flex flex-row gap-2'>
-      <h1>Your luxury</h1>
-      <h1>concierge, awaits.</h1>
-      </div>
-      <div>
-        <button className='px-4 py-2 bg-white text-gray-800'>BOOK YOUR CONSULTATION</button>
-      </div>
+
+      <h1 className='md:text-5xl text-3xl text-white text-center md:text-left'>Your luxury <br className='md:block hidden' />concierge, awaits.</h1>
+
+
+
+      <button className='px-4 py-2 bg-white text-gray-800'>BOOK YOUR CONSULTATION</button>
+
     </div>
   )
 }
