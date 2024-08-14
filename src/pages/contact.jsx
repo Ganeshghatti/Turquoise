f01f01eb305fbde83a74a3e93af4d120d47f0e69
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-14 18:19:44 +0530

    added chat icon

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ContactForm from '../components/ContactForm'
 // import ContactFooter from '../components/ContactFooter'
+import { CiChat1 } from "react-icons/ci";
 
 const Contact = () => {
+    const phoneNumber = '+91 94496 10077'; 
+
+    
+
     return (
         <section>
-
             {/* <ContactFooter /> */}
             <div className='text-center py-24 my-24 px-5'>
                 <p className='lg:text-xl family-montserrat'>Carpentum Lifestyle Management</p>
@@ -16,13 +20,19 @@ const Contact = () => {
                 <img src="./contact-bg.jpg" alt="contact" className='object-cover object-center' loading='lazy' />
             </div> */}
 
-
             <ContactForm />
-           
 
-
+            {/* Chat Button */}
+            <a
+                className='fixed bottom-8 right-8 w-12 h-12 bg-[#149088] text-white flex items-center justify-center  rounded-full cursor-pointer animate-bounce' 
+               
+                href='https://wa.me/'
+                target='_blank'
+            >
+              <CiChat1 size={25} />
+            </a>
         </section>
     )
 }
 
-export default Contact
+export default Contact;
