00f6276fd1631e3a599f6c5e2dc9267086a1005d
Ganeshghatti <ghattiganesh8@gmail.com>
2024-08-15 11:01:45 +0530

    minor changes

--- src/components/value-card.jsx
+++ src/components/value-card.jsx
@@ -1,16 +1,27 @@
-import { SlSettings } from "react-icons/sl";
+import { MdLightbulbOutline } from "react-icons/md";
+import { FaFire } from "react-icons/fa";
+import { PiMedalLight } from "react-icons/pi";
 
 // eslint-disable-next-line react/prop-types
-const ValueCard = ({ title, subtitle, desc }) => {
+const ValueCard = ({ title, desc, icon }) => {
+  const iconMap = {
+    MdLightbulbOutline: (
+      <MdLightbulbOutline className="h-20 w-20 text-secondary" />
+    ),
+    FaFire: <FaFire className="h-20 w-20 text-secondary" />,
+    PiMedalLight: <PiMedalLight className="h-20 w-20 text-secondary" />,
+  };
+
   return (
     <div className="flex h-[27.938rem] w-[23.48rem] flex-col space-y-3 border-t-4 border-t-secondary p-5 pt-10 shadow-lg">
-      <SlSettings className="h-24 w-24 text-secondary" />
-      <h1 className="family-montserrat text-2xl md:text-[1.75rem] font-semibold text-primary">
+      <div>{iconMap[icon]}</div>
+
+      <h1 className="family-montserrat text-2xl font-semibold text-primary md:text-[1.75rem]">
         {title}
       </h1>
-      <h3 className="family-mplus text-base md:text-[1.125rem] font-normal uppercase text-secondary">
+      {/* <h3 className="family-mplus text-base md:text-[1.125rem] font-normal uppercase text-secondary">
         {subtitle}
-      </h3>
+      </h3> */}
       <p className="family-montserrat text-[0.9rem] font-normal text-gray-700">
         {desc}
       </p>
