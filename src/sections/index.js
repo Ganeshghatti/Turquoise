ffa391538f93d40db173698d01bee9e323003f3c
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-11-20 20:08:33 +0530

    Add : add review section

--- src/sections/index.js
+++ src/sections/index.js
@@ -4,3 +4,4 @@ export { default as Values } from "./values";
 export { default as Consultation } from "./consultation";
 export { default as Gallery } from "./gallery";
 export { default as Partners } from "./partners";
+export { default as Reviews } from "./review";
